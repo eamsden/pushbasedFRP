7b59270b81d442c12f1261b9e241e5eef68f71b6
Edward Amsden <edwardamsden@gmail.com>
2011-10-23 21:06:27 -0400

    RF GADT

--- Code/TypeFamilies/ReactiveFunctions.hs
+++ Code/TypeFamilies/ReactiveFunctions.hs
@@ -1,49 +1,52 @@
 {-# LANGUAGE GADTs, MultiParamTypeClasses, RankNTypes, FlexibleInstances, FunctionalDependencies #-}
 module ReactiveFunctions where
 
-import Routes
 import ReactiveVectors
 
--- Event pushing
-class PushEvent rf v1 v2 
-  pushEvent :: rf v1 v2 -> EventRoute v1 -> (rf v1 v2, EventRoute v2)
-  
--- Wrapper type for reactive functions
-data RF v1 v2 = forall c . PushEvent c v1 v2 => RF (c v1 v2)
-
-instance PushEvent RF v1 v2 where
-  pushEvent (RF f) r = let (f', r') = pushEvent f r 
-                       in (RF f', r')
-
--- Reactive Function constructors
-data RFPureSignal v1 v2 where
-  RFPureSignal :: (a -> b) -> RFPureSignal (SVSignal a) (SVSignal b)
-  
-data RFPureEvent v1 v2 where
-  RFPureEvent :: (a -> b) -> RFPureEvent (SVEvent a) (SVEvent b)
-  
-data RFSequence v1 v2 where
-  RFSequence :: RF v1 v2 -> RF v2 v3 -> RFSequence v1 v3
-  
-data RFCopy v1 v2 where
-  RFCopy :: RFCopy v1 (SVAppend v1 v1)
-
+data RF v1 v2 where
+  RFPureSignal :: (a -> b) -> RF (RVSignal a) (RVSignal b)
+  RFPureEvent  :: (a -> b) -> RF (RVEvent a) (RVEvent b)
+  RFSwitch     :: RF v1 (RVAppend v2 (RVEvent a)) -> (a -> RF v1 v2) -> RF v1 v2
+  RFSequence   :: RF v1 v2 -> RF v2 v3 -> RF v1 v3
+  RFPass       :: RF v1 v2 -> RF (RVAppend v1 v3) (RVAppend v2 v3)
+  RFCancelL    :: RF (RVAppend RVEmpty v1) v1
+  RFUncancelL  :: RF v1 (RVAppend RVEmpty v1)
+  RFAssoc      :: RF (RVAppend (RVAppend v1 v2) v3) (RVAppend v1 (RVAppend v2 v3))
+  RFUnassoc    :: RF (RVAppend v1 (RVAppend v2 v3)) (RVAppend (RVAppend v1 v2) v3)
+  RFCopy       :: RF v1 (RVAppend v1 v1)
+  RFDrop       :: RF v1 RVEmpty
+  RFSwap       :: RF (RVAppend v1 v2) (RVAppend v2 v1)
   
 -- Event pushing for reactive functions
-instance PushEvent RFPureSignal (SVSignal a) (SVSignal b) (EventRoute (SVSignal a)) (EventRoute (SVSignal b))where
-  pushEvent sf _ = (sf, EventRouteNowhere)
-  
-instance PushEvent RFPureEvent (SVEvent a) (SVEvent b) where
-  pushEvent ef EventRouteDoneNowhere = (ef, EventRouteDoneNowhere)
-  pushEvent ef@(RFPureEvent f) (EventRouteDone x) = (ef, EventRouteDone (f x))
-  
-instance PushEvent RFSequence v1 v2 where
-  pushEvent (RFSequence rf1 rf2) r = let (rf1', r') = pushEvent rf1 r
-                                         (rf2', r'') = pushEvent rf2 r'
-                                     in (RFSequence rf1' rf2', r'')
-{-                                        
--- Fails with "no instance for EventRoutesCombine (EventRoute v1) (EventRoute v1)"
-instance PushEvent RFCopy v1 (SVAppend v1 v1) where
-  pushEvent rf r = (rf, combineEventRoutes r r)
--}
-  
+pushEvent :: RF v1 v2 -> EventRoute v1 (EventRouteType v1) -> (RF v1 v2, EventRoute v2 (EventRouteType v2))
+pushEvent sf@(RFPureSignal _) _ = (sf, EventRouteSignal)
+pushEvent ef@(RFPureEvent _) EventRouteNoEvent = (ef, EventRouteNoEvent)
+pushEvent ef@(RFPureEvent f) (EventRouteEvent x) = (ef, EventRouteEvent (f x))
+pushEvent sw@(RFSwitch rf f) r = let (rf', r') = pushEvent rf r
+                                 in case (eventRouteSecond r') of
+                                     EventRouteEvent x -> (f x, eventRouteFirst r')
+                                     EventRouteNoEvent -> (RFSwitch rf' f, eventRouteFirst r')
+pushEvent (RFSequence rf1 rf2) r = let (rf1', r') = pushEvent rf1 r
+                                       (rf2', r'') = pushEvent rf2 r'
+                                   in (RFSequence rf1' rf2', r'')
+pushEvent (RFPass rf) r = let (rf', rFirst') = pushEvent rf (eventRouteFirst r)
+                          in (RFPass rf', combineEventRoute rFirst' (eventRouteSecond r))
+pushEvent RFCancelL r = (RFCancelL, eventRouteSecond r)
+pushEvent RFUncancelL r = (RFUncancelL, combineEventRoute EventRouteEmpty r)
+pushEvent RFAssoc r = (RFAssoc, let rp = eventRouteFirst r
+                                    r1 = eventRouteFirst rp
+                                    r2 = eventRouteSecond rp
+                                    r3 = eventRouteSecond r
+                                in combineEventRoute r1 (combineEventRoute r2 r3))
+pushEvent RFUnassoc r = (RFUnassoc, let r1 = eventRouteFirst r
+                                        rp = eventRouteSecond r
+                                        r2 = eventRouteFirst rp
+                                        r3 = eventRouteSecond rp
+                                    in combineEventRoute (combineEventRoute r1 r2) r3)
+pushEvent RFCopy r = (RFCopy, combineEventRoute r r)
+pushEvent RFDrop _ = (RFDrop, EventRouteEmpty)
+pushEvent RFSwap r = (RFSwap, combineEventRoute (eventRouteSecond r) (eventRouteFirst r)) 
+
+
+
+
--- Code/TypeFamilies/ReactiveVectors.hs
+++ Code/TypeFamilies/ReactiveVectors.hs
@@ -1,15 +1,70 @@
-{-# LANGUAGE EmptyDataDecls, TypeFamilies, UndecidableInstances #-}
+{-# LANGUAGE EmptyDataDecls, TypeFamilies, GADTs, UndecidableInstances #-}
 module ReactiveVectors where
 
-import Routes
+-- Routes
+data RouteNowhere
+data RouteDone a                           
+data RouteFirst r                           
+data RouteSecond r                           
+data RouteBoth r1 r2
+                            
+type family CombineRouteType r1 r2
+type instance CombineRouteType RouteNowhere RouteNowhere = RouteNowhere
+type instance CombineRouteType RouteNowhere (RouteDone a) = RouteSecond (RouteDone a)
+type instance CombineRouteType RouteNowhere (RouteFirst r) = RouteSecond (RouteFirst r)
+type instance CombineRouteType RouteNowhere (RouteSecond r) = RouteSecond (RouteSecond r)
+type instance CombineRouteType RouteNowhere (RouteBoth r1 r2) = RouteSecond (RouteBoth r1 r2)
+type instance CombineRouteType (RouteDone a) RouteNowhere = RouteFirst (RouteDone a)
+type instance CombineRouteType (RouteFirst r) RouteNowhere = RouteFirst (RouteFirst r)
+type instance CombineRouteType (RouteSecond r) RouteNowhere = RouteFirst (RouteSecond r)
+type instance CombineRouteType (RouteBoth r1 r2) RouteNowhere = RouteFirst (RouteBoth r1 r2)
+type instance CombineRouteType (RouteDone a) (RouteDone b) = RouteBoth (RouteDone a) (RouteDone b)
+type instance CombineRouteType (RouteDone a) (RouteFirst r) = RouteBoth (RouteDone a) (RouteFirst r)
+type instance CombineRouteType (RouteDone a) (RouteSecond r) = RouteBoth (RouteDone a) (RouteSecond r)
+type instance CombineRouteType (RouteDone a) (RouteBoth r1 r2) = RouteBoth (RouteDone a) (RouteBoth r1 r2)
+type instance CombineRouteType (RouteFirst r) (RouteDone b) = RouteBoth (RouteFirst r) (RouteDone b)
+type instance CombineRouteType (RouteFirst r1) (RouteFirst r2) = RouteBoth (RouteFirst r1) (RouteFirst r2)
+type instance CombineRouteType (RouteFirst r1) (RouteSecond r2) = RouteBoth (RouteFirst r1) (RouteSecond r2)
+type instance CombineRouteType (RouteFirst r1) (RouteBoth r2 r3) = RouteBoth (RouteFirst r1) (RouteBoth r2 r3)
+type instance CombineRouteType (RouteSecond r) (RouteDone b) = RouteBoth (RouteSecond r) (RouteDone b)
+type instance CombineRouteType (RouteSecond r1) (RouteFirst r2) = RouteBoth (RouteSecond r1) (RouteFirst r2)
+type instance CombineRouteType (RouteSecond r1) (RouteSecond r2) = RouteBoth (RouteSecond r1) (RouteSecond r2)
+type instance CombineRouteType (RouteSecond r1) (RouteBoth r2 r3) = RouteBoth (RouteSecond r1) (RouteBoth r2 r3)
+type instance CombineRouteType (RouteBoth r1 r2) (RouteDone b) = RouteBoth (RouteBoth r1 r2) (RouteDone b)
+type instance CombineRouteType (RouteBoth r1 r2) (RouteFirst r3) = RouteBoth (RouteBoth r1 r2) (RouteFirst r3)
+type instance CombineRouteType (RouteBoth r1 r2) (RouteSecond r3) = RouteBoth (RouteBoth r1 r2) (RouteSecond r3)
+type instance CombineRouteType (RouteBoth r1 r2) (RouteBoth r3 r4) = RouteBoth (RouteBoth r1 r2) (RouteBoth r3 r4)
+     
+-- Reactive Vectors
+data RVEmpty
+data RVSignal a
+data RVEvent a
+data RVAppend v1 v2
 
-data SVEmpty
-data SVSignal a
-data SVEvent a
-data SVAppend v1 v2
 
-type family EventRoute v
-type instance EventRoute SVEmpty = EventRouteNowhere
-type instance EventRoute (SVSignal a) = EventRouteNowhere
-type instance EventRoute (SVEvent a) = EventRouteDone a
-type instance EventRoute (SVAppend v1 v2) = CombineEventRoutes (EventRoute v1) (EventRoute v2)
+type family EventRouteType v
+type instance EventRouteType RVEmpty          = RouteNowhere
+type instance EventRouteType (RVSignal a)     = RouteNowhere
+type instance EventRouteType (RVEvent a)      = RouteDone a
+type instance EventRouteType (RVAppend v1 v2) = CombineRouteType (EventRouteType v1) (EventRouteType v2)
+
+data EventRoute v r where
+  EventRouteEmpty          :: EventRoute RVEmpty RouteNowhere
+  EventRouteSignal         :: EventRoute (RVSignal a) RouteNowhere
+  EventRouteEvent          :: a -> EventRoute (RVEvent a) (RouteDone a)
+  EventRouteNoEvent        :: EventRoute (RVEvent a) (RouteDone a)
+  EventRouteCombine        :: EventRoute v1 (EventRouteType v1) 
+                              -> EventRoute v2 (EventRouteType v2) 
+                              -> EventRoute (RVAppend v1 v2) (CombineRouteType (EventRouteType v1) (EventRouteType v2))
+
+  
+combineEventRoute :: EventRoute v1 (EventRouteType v1) 
+                     -> EventRoute v2 (EventRouteType v2) 
+                     -> EventRoute (RVAppend v1 v2) (CombineRouteType (EventRouteType v1) (EventRouteType v2))
+combineEventRoute r1 r2 = EventRouteCombine r1 r2
+
+eventRouteFirst :: EventRoute (RVAppend v1 v2) (CombineRouteType (EventRouteType v1) (EventRouteType v2)) -> EventRoute v1 (EventRouteType v1)
+eventRouteFirst (EventRouteCombine r1 _) = r1
+
+eventRouteSecond :: EventRoute (RVAppend v1 v2) (CombineRouteType (EventRouteType v1) (EventRouteType v2)) -> EventRoute v2 (EventRouteType v2)
+eventRouteSecond (EventRouteCombine _ r2) = r2
